79bb55a72c815f22d0cb5c877f23cc36ace3388a
DxAmbar <d.ambarbaeza@gmail.com>
2023-01-05 16:38:23 -0300

    Empezando maquetación

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 
-export function GifExpertApp() {
-  return (
 
-    <h1>GifExpertApp</h1>
+export const GifExpertApp = () => {
+
+    const [categories, setcategories] = useState(['One Punch', 'Dragon Ball'])
+
+    return (
+        <>
+            {/* título */}
+            <h1>GifExpertApp</h1>
+
+            {/* input */}
+
+            {/* listado de Gif */}
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+                {/* <li></li> */}
+            </ol>
+
+            {/* Gif Item */}
+
     
-  )
+        </>
+    )
 }
 
