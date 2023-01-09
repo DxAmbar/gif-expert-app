d883da4e37929287ab53ed75c5cf7e070662f0ff
DxAmbar <d.ambarbaeza@gmail.com>
2023-01-09 19:53:58 -0300

    Se muestran gifs en pantalla

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 
 export const GifGrid = ({ category }) => {
@@ -20,13 +21,19 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{ category }</h3>
 
-            <ol>
+            <div className="card-grid">
                 {
-                    images.map( ({ id, title }) => (
-                        <li key={ id }>{ title }</li>
+                    images.map( ( image ) => (
+                        <GifItem 
+                            key={ image.id }
+                            /* title={ image.title }
+                            url={ image.url } o:*/
+                            { ...image} //todas las propiedades de image
+
+                            />
                     ))
                 }
-            </ol>
+            </div>
 
         </>
     )
